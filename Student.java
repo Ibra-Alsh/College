71c94d835151235d7d231dcc162b288faf8a0d6d
Ibra-Alsh <155462862+Ibra-Alsh@users.noreply.github.com>
2026-04-02 01:17:59 +0300

    Refactor Student class GPA methods
    Refactor GPA calculation methods and update isReward method.

--- Student.java
+++ Student.java
@@ -3,19 +3,18 @@ public abstract class Student extends Department implements reward {
     private String name;
     private String id ;
 
-public Student (String name , String id ){
-    super();
-    this.name=name;
-    this.id=id;
-}
-protected abstract double calculate_GPA();
+    public Student (String name , String id ){
+        super();
+        this.name=name;
+        this.id=id;
+    }
+
+    protected abstract double calculateGPA(double totalGradePoints, double totalCreditHours);
+
+    public abstract double calculateGPA();
 
     @Override
-    public boolean isReward() {
-        if (calculate_GPA()>4.75)
-        return true;
-        else return false;
-    }
+    public abstract boolean isReward();
 
     @Override
     public String toString() {
